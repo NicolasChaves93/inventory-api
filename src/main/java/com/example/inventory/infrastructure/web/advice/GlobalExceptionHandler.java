1a5afc9542691695f16a074e08f20a14a9e5bb24
Nicolas Chaves <nicolas.chaves93@gmail.com>
2025-08-24 02:22:20 -0500

    Ajustar versiones de las depedencias del proyecto

--- src/main/java/com/example/inventory/infrastructure/web/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/inventory/infrastructure/web/advice/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
 package com.example.inventory.infrastructure.web.advice;
 
 import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
